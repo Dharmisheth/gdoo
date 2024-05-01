a8915549cd4bddb6f3a01197b3733bf0397f3549
Dharmi Sheth <91783686+Dharmisheth@users.noreply.github.com>
2024-05-01 20:04:04 +0530

    ""

--- src/app/Home/page.js
+++ src/app/Home/page.js
@@ -3,8 +3,8 @@ import React from "react";
 function page() {
   return (
     <>
-      <div className="main">
-        <div className="container overflow-visible">
+      <div className="main main overflow-auto">
+        <div className="container">
           <div className="flex justify-between px-5 pt-14 ">
             <p className="text-3xl font-bold">Aries, Welcome to Gdooo</p>
             <svg
@@ -191,60 +191,60 @@ function page() {
             </div>
           </div>
 
-<div className="">
-          <div className="px-5">
-            <div className="grid justify-center gap-2 pt-8">
-              <p className="font-semibold text-xl">Visit Popup Store</p>
-              <div className="grid grid-flow-col gap-4">
-                <img src="/Rectangle.png"></img>
-                <img src="/Rectangle2.png"></img>
+          <div className="">
+            <div className="px-5">
+              <div className="grid justify-center gap-2 pt-8">
+                <p className="font-semibold text-xl">Visit Popup Store</p>
+                <div className="grid grid-flow-col gap-4">
+                  <img src="/Rectangle.png"></img>
+                  <img src="/Rectangle2.png"></img>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className="px-5">
-            <div className="grid justify-center gap-2 pt-6">
-              <p className="font-semibold text-xl">Play Gacha</p>
-              <div className="grid grid-flow-col gap-4">
-                <img src="/Rectangle3.png"></img>
-                <img src="/Rectangle4.png"></img>
+            <div className="px-5">
+              <div className="grid justify-center gap-2 pt-6">
+                <p className="font-semibold text-xl">Play Gacha</p>
+                <div className="grid grid-flow-col gap-4">
+                  <img src="/Rectangle3.png"></img>
+                  <img src="/Rectangle4.png"></img>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className="px-5">
-            <div className="grid justify-center gap-2 pt-6">
-              <p className="font-semibold text-xl">Latest Products</p>
-              <div className="relative grid grid-flow-col gap-4">
-                <img src="/Rectangle5.png"></img>
-                <img src="/Rectangle6.png"></img>
+            <div className="px-5">
+              <div className="grid justify-center gap-2 pt-6">
+                <p className="font-semibold text-xl">Latest Products</p>
+                <div className="relative grid grid-flow-col gap-4">
+                  <img src="/Rectangle5.png"></img>
+                  <img src="/Rectangle6.png"></img>
+                </div>
               </div>
             </div>
           </div>
-          </div>
-          
 
           <div className="flex justify-center">
             <div className="absolute flex justify-center -bottom-[414px]">
-            <button className="rounded-full  bg-black size-12 flex items-center justify-center">
-              <svg className=""
-                width="18"
-                height="20"
-                viewBox="0 0 18 20"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M4 9V6C4 3.23858 6.23858 1 9 1C11.419 1 13.4367 2.71776 13.9 5M5.8 19H12.2C13.8802 19 14.7202 19 15.362 18.673C15.9265 18.3854 16.3854 17.9265 16.673 17.362C17 16.7202 17 15.8802 17 14.2V13.8C17 12.1198 17 11.2798 16.673 10.638C16.3854 10.0735 15.9265 9.6146 15.362 9.32698C14.7202 9 13.8802 9 12.2 9H5.8C4.11984 9 3.27976 9 2.63803 9.32698C2.07354 9.6146 1.6146 10.0735 1.32698 10.638C1 11.2798 1 12.1198 1 13.8V14.2C1 15.8802 1 16.7202 1.32698 17.362C1.6146 17.9265 2.07354 18.3854 2.63803 18.673C3.27976 19 4.11984 19 5.8 19Z"
-                  stroke="white"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
-            </button>
+              <button className="rounded-full  bg-black size-12 flex items-center justify-center">
+                <svg
+                  className=""
+                  width="18"
+                  height="20"
+                  viewBox="0 0 18 20"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M4 9V6C4 3.23858 6.23858 1 9 1C11.419 1 13.4367 2.71776 13.9 5M5.8 19H12.2C13.8802 19 14.7202 19 15.362 18.673C15.9265 18.3854 16.3854 17.9265 16.673 17.362C17 16.7202 17 15.8802 17 14.2V13.8C17 12.1198 17 11.2798 16.673 10.638C16.3854 10.0735 15.9265 9.6146 15.362 9.32698C14.7202 9 13.8802 9 12.2 9H5.8C4.11984 9 3.27976 9 2.63803 9.32698C2.07354 9.6146 1.6146 10.0735 1.32698 10.638C1 11.2798 1 12.1198 1 13.8V14.2C1 15.8802 1 16.7202 1.32698 17.362C1.6146 17.9265 2.07354 18.3854 2.63803 18.673C3.27976 19 4.11984 19 5.8 19Z"
+                    stroke="white"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </button>
             </div>
-            </div>
+          </div>
 
           <div className="rounded-t-xl bg-[#FD9318] box px-1">
             <div className="pt-[9.75px] gap-1">
--- src/app/page.js
+++ src/app/page.js
@@ -1,70 +1,22 @@
-import Head from "next/head";
-import React from "react";
+import React from 'react'
 
-const SignUp = () => {
+function Gdooo() {
   return (
     <>
-      <Head>
-        <title>Sign Up</title>
-      </Head>
       <div className="main">
-        <div className="container gap-6">
-          <div className="abc">
-            <div className="pt-3 pb-6">
-              <button className="btn cursor-pointer rounded-full size-10 text-justify grid place-items-center gap-3">
-                <svg
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M15 18L9 12L15 6"
-                    stroke="white"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                </svg>
-              </button>
-            </div>
-            <h1 className="text-3xl font-bold gap-5 pb-3">Sign up</h1>
-            <p style={{ color: "#6D6D6D" }} className="text-sm pb-5">
-              Please enter your legal name. This information will be used to
-              verify your account.
-            </p>
-            <div className="">
-              <form className="flex flex-col justify-between">
-                <label className="text-sm font-semibold pb-1">Name</label>
-                <input
-                  placeholder="Aries"
-                  className="block rounded-full px-3 py-2.5 "
-                ></input>
-
-                <label className="text-sm font-semibold pb-1 pt-5">Phone</label>
-                <input
-                  placeholder="US      +1"
-                  type="number"
-                  className="block rounded-full px-3 py-2.5 "
-                ></input>
-                <div className="flex flex-col justify-between pt-72 pb-9">
-                  <button
-                    type="submit"
-                    className="submit rounded-full px-3 py-2.5"
-                  >
-                    <p className="text-white text-sm font-semibold grid items-center">
-                      Continue
-                    </p>
-                  </button>
+        <div className="container2 relative">
+              <div className='bcd flex-col flex items-center'>
+                <img src="/App_Store.png" className=''></img>
+                <div  className='text-white font-extrabold text-6xl text-center items-center'>
+                <p>Welcome</p>
+                <p>to</p>
+                <p>Gdoo!</p>
                 </div>
-              </form>
+              </div>
             </div>
-          </div>
-        </div>
-      </div>
+            </div>
     </>
-  );
-};
+  )
+}
 
-export default SignUp;
+export default Gdooo
